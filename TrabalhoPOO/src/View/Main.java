c660825febc304fda85ac9f68d5b9599a090e07d
KreutzVinicius <viniciuskr2010@gmail.com>
2020-09-15 09:02:47 -0300

    done

--- TrabalhoPOO/src/View/Main.java
+++ TrabalhoPOO/src/View/Main.java
@@ -1,4 +1,4 @@
-package Apresentacao;
+package View;
 
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -32,22 +32,26 @@ import Dados.Medico;
 import Dados.Paciente;
 import Dados.Pessoa;
 import Negocios.Sistema;
+import Persistencia.ConsultaDAO;
 import Persistencia.MedicoDAO;
 import Persistencia.PacienteDAO;
 import exceptions.SelectException;
 import javax.swing.JComboBox;
 
-public class Interface {
+public class Main {
 
 	static Sistema sistema = new Sistema();
 	
 	private JFrame frmHospitalSystem;
 	
+	private JTextField nomeField;
 	private JTextField valorField;
-	private JTextField idadeField;
 	private JTextField cpfField;
+	private JTextField idadeField;
 	private JTextField cidadeField;
 	private JTextField descricaoField;
+	private JTextField horarioField;
+	private JTextField diagnosticoField;
 	
 	JTable tblPacientes = new JTable();
 	JTable tblMedicos = new JTable();
@@ -55,10 +59,10 @@ public class Interface {
 	
 	JComboBox comboMedico;
 	JComboBox comboPaciente;
+	JComboBox comboConsulta;
 	
 
-	private JTextField horarioField;
-	private JTextField diagnosticoField;
+	
 	
 	
 
@@ -69,7 +73,7 @@ public class Interface {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Interface window = new Interface();
+					Main window = new Main();
 					window.frmHospitalSystem.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -81,7 +85,7 @@ public class Interface {
 	/**
 	 * Create the application.
 	 */
-	public Interface() {
+	public Main() {
 		initialize();
 	}
 
@@ -89,8 +93,6 @@ public class Interface {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-
-		
 		
 		frmHospitalSystem = new JFrame();
 		frmHospitalSystem.setTitle("Hospital System");
@@ -98,8 +100,9 @@ public class Interface {
 		frmHospitalSystem.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHospitalSystem.setLocationRelativeTo(null);
 		frmHospitalSystem.getContentPane().setLayout(null);
+
 		
-			
+		
 		JButton btnCadastrarPaciente = new JButton("Cadastar Paciente");
 		btnCadastrarPaciente.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnCadastrarPaciente.addMouseListener(new MouseAdapter() {
@@ -119,11 +122,11 @@ public class Interface {
 				frmHospitalSystem.getContentPane().add(panel);
 				panel.setLayout(null);
 				
-				valorField = new JTextField();
-				valorField.setBounds(10, 29, 800, 33);
-				panel.add(valorField);
-				valorField.setColumns(10);
-				valorField.setText("Nome");
+				nomeField = new JTextField();
+				nomeField.setBounds(10, 29, 800, 33);
+				panel.add(nomeField);
+				nomeField.setColumns(10);
+				nomeField.setText("Nome");
 				
 				idadeField = new JTextField();
 				idadeField.setColumns(10);
@@ -158,7 +161,7 @@ public class Interface {
 						 String aux;
 						 
 						 
-						 pessoa.setNome(valorField.getText());
+						 pessoa.setNome(nomeField.getText());
 						 aux = idadeField.getText();
 						 int idade = Integer.parseInt(aux);
 						 pessoa.setIdade(idade);
@@ -202,11 +205,11 @@ public class Interface {
 				frmHospitalSystem.getContentPane().add(panel);
 				panel.setLayout(null);
 				
-				valorField = new JTextField();
-				valorField.setBounds(10, 29, 800, 33);
-				panel.add(valorField);
-				valorField.setColumns(10);
-				valorField.setText("Nome");
+				nomeField = new JTextField();
+				nomeField.setBounds(10, 29, 800, 33);
+				panel.add(nomeField);
+				nomeField.setColumns(10);
+				nomeField.setText("Nome");
 				
 				idadeField = new JTextField();
 				idadeField.setColumns(10);
@@ -240,7 +243,7 @@ public class Interface {
 						 String aux;
 						 
 						 
-						 pessoa.setNome(valorField.getText());
+						 pessoa.setNome(nomeField.getText());
 						 aux = idadeField.getText();
 						 int idade = Integer.parseInt(aux);
 						 pessoa.setIdade(idade);
@@ -334,9 +337,7 @@ public class Interface {
 					public void mouseClicked(MouseEvent e) {
 						Consulta consulta = new Consulta();
 						 String aux;
-						 
-						 
-	
+
 						 aux = valorField.getText();
 						 int valor = Integer.parseInt(aux);
 						 consulta.setValor(valor);
@@ -453,6 +454,34 @@ public class Interface {
 		btnListarConsultas.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				JPanel panel = new JPanel();
+				panel.setBounds(140, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(panel);
+				panel.setLayout(null);
+
+
+				tblConsultas.setBorder(new LineBorder(new Color(0, 0, 0)));
+				tblConsultas.setModel(new DefaultTableModel(
+					new Object[][] {
+					},
+					new String[] {
+						"ID","Valor", "Horario", "ID Medico", "ID Paciente"
+					}
+				));
+				tblConsultas.setBounds(64, 67, 546, 178);
+				
+				JScrollPane scrollPane = new JScrollPane(tblConsultas);
+				scrollPane.setBounds(22, 11, 788, 385);
+				panel.add(scrollPane);
+				scrollPane.setViewportView(tblConsultas);
+				ArrayList<Consulta> lista = null;
+				try {
+					lista = ConsultaDAO.getInstance().selectAllCon();
+				} catch (SelectException e1) {
+					e1.printStackTrace();
+				}
+				atualizaTabelaC(lista);
+				
 			}
 		});
 		btnListarConsultas.setBounds(10, 275, 120, 30);
@@ -463,6 +492,83 @@ public class Interface {
 		btnAlterarPaciente.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				JPanel AlterarPanel = new JPanel();
+				AlterarPanel.setVisible(true);
+				
+				LayoutManager ol_AlterarPanel = new OverlayLayout(AlterarPanel);
+				AlterarPanel.setLayout(ol_AlterarPanel);
+				JButton btnAlterar = new JButton("Alterar");
+				AlterarPanel.add(btnAlterar);
+				btnAlterar.setVisible(true);
+				
+				nomeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(nomeField);
+				nomeField.setBounds(163, 192, 800, 33);
+				nomeField.setColumns(10);
+				nomeField.setText("Nome");
+				
+				idadeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(idadeField);
+				idadeField.setColumns(10);
+				idadeField.setBounds(163, 231, 800, 33);
+				idadeField.setText("Idade");
+				
+				cidadeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(cidadeField);
+				cidadeField.setColumns(10);
+				cidadeField.setBounds(163, 274, 800, 33);
+				cidadeField.setText("Cidade");
+				
+				descricaoField = new JTextField();
+				frmHospitalSystem.getContentPane().add(descricaoField);
+				descricaoField.setColumns(10);
+				descricaoField.setBounds(163, 316, 800, 170);
+				descricaoField.setText("Descri\u00E7\u00E3o\r\n\r\n\r\n\r\n\r\n");
+				
+				AlterarPanel.setBounds(154, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(AlterarPanel);
+				AlterarPanel.setLayout(null);
+				
+				comboPaciente = new JComboBox();
+				comboPaciente.setBounds(10, 10, 330, 30);
+				AlterarPanel.add(comboPaciente);
+				
+				
+				List<Paciente> pacientes = null;
+				try {
+					pacientes = PacienteDAO.getInstance().selectAllPac();
+				} catch (SelectException e2) {
+					e2.printStackTrace();
+				} 
+				comboPaciente.setModel(new ComboBoxModelAll(pacientes));
+				
+				
+				btnAlterar.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					Paciente paciente = new Paciente();
+					String aux;
+					paciente = (Paciente) comboPaciente.getModel().getSelectedItem();
+						
+					paciente.setNome(nomeField.getText());
+					 aux = idadeField.getText();
+					 int idade = Integer.parseInt(aux);
+					 paciente.setIdade(idade);
+					 paciente.setCidade(cidadeField.getText());
+					 
+					 paciente.setDescricao(descricaoField.getText());
+					
+					sistema.alterarPaciente(paciente);
+					sistema.alterarPessoa(paciente);
+					
+						JOptionPane.showMessageDialog(null, "Alteração efetuada com sucesso !");
+						AlterarPanel.setVisible(false);
+						btnAlterar.setVisible(false);
+					}
+				});
+				btnAlterar.setBounds(686, 422, 124, 40);
 			}
 		});
 		btnAlterarPaciente.setBounds(10, 316, 120, 30);
@@ -473,6 +579,82 @@ public class Interface {
 		btnAlterarMedico.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				JPanel AlterarPanel = new JPanel();
+				AlterarPanel.setVisible(true);
+				
+				LayoutManager ol_AlterarPanel = new OverlayLayout(AlterarPanel);
+				AlterarPanel.setLayout(ol_AlterarPanel);
+				JButton btnAlterar = new JButton("Alterar");
+				AlterarPanel.add(btnAlterar);
+				btnAlterar.setVisible(true);
+				
+				nomeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(nomeField);
+				nomeField.setBounds(163, 192, 800, 33);
+				nomeField.setColumns(10);
+				nomeField.setText("Nome");
+				
+				idadeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(idadeField);
+				idadeField.setColumns(10);
+				idadeField.setBounds(163, 231, 800, 33);
+				idadeField.setText("Idade");
+				
+				cidadeField = new JTextField();
+				frmHospitalSystem.getContentPane().add(cidadeField);
+				cidadeField.setColumns(10);
+				cidadeField.setBounds(163, 274, 800, 33);
+				cidadeField.setText("Cidade");
+				
+				descricaoField = new JTextField();
+				frmHospitalSystem.getContentPane().add(descricaoField);
+				descricaoField.setColumns(10);
+				descricaoField.setBounds(163, 316, 800, 170);
+				descricaoField.setText("Descri\u00E7\u00E3o\r\n\r\n\r\n\r\n\r\n");
+				
+				AlterarPanel.setBounds(154, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(AlterarPanel);
+				AlterarPanel.setLayout(null);
+				
+				comboMedico = new JComboBox();
+				comboMedico.setBounds(10, 10, 330, 30);
+				AlterarPanel.add(comboMedico);
+				
+				
+				List<Medico> medicos = null;
+				try {
+					medicos = MedicoDAO.getInstance().selectAllMed();
+				} catch (SelectException e2) {
+					e2.printStackTrace();
+				} 
+				comboMedico.setModel(new ComboBoxModelAll(medicos));
+				
+				
+				btnAlterar.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					Medico medico = new Medico();
+					String aux;
+					medico = (Medico) comboMedico.getModel().getSelectedItem();
+						
+					medico.setNome(nomeField.getText());
+					 aux = idadeField.getText();
+					 int idade = Integer.parseInt(aux);
+					 medico.setIdade(idade);
+					 medico.setCidade(cidadeField.getText());
+					 
+					 medico.setEspecialidade(descricaoField.getText());
+					
+					sistema.alterarMedico(medico);
+					sistema.alterarPessoa(medico);
+					
+						JOptionPane.showMessageDialog(null, "Alteração efetuada com sucesso !");
+						AlterarPanel.setVisible(false);
+						btnAlterar.setVisible(false);
+					}
+				});
+				btnAlterar.setBounds(686, 422, 124, 40);
 			}
 		});
 		btnAlterarMedico.setBounds(10, 357, 120, 30);
@@ -483,6 +665,82 @@ public class Interface {
 		btnAlterarConsulta.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				JPanel AlterarPanel = new JPanel();
+				AlterarPanel.setVisible(true);
+				
+				LayoutManager ol_AlterarPanel = new OverlayLayout(AlterarPanel);
+				AlterarPanel.setLayout(ol_AlterarPanel);
+				JButton btnAlterar = new JButton("Alterar");
+				AlterarPanel.add(btnAlterar);
+				btnAlterar.setVisible(true);
+				
+				diagnosticoField = new JTextField();
+				frmHospitalSystem.getContentPane().add(diagnosticoField);
+				diagnosticoField.setText("Diagnostico");
+				diagnosticoField.setColumns(10);
+				diagnosticoField.setBounds(161, 219, 800, 108);
+				
+				horarioField = new JTextField();
+				frmHospitalSystem.getContentPane().add(horarioField);
+				horarioField.setText("00/00/0000 00:00");
+				horarioField.setColumns(10);
+				horarioField.setBounds(161, 175, 800, 33);
+				
+				valorField = new JTextField();
+				frmHospitalSystem.getContentPane().add(valorField);
+				valorField.setBounds(161, 131, 800, 33);
+				valorField.setColumns(10);
+				valorField.setText("Valor");
+				
+				AlterarPanel.setBounds(154, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(AlterarPanel);
+				AlterarPanel.setLayout(null);
+				
+				comboConsulta = new JComboBox();
+				comboConsulta.setBounds(10, 10, 330, 30);
+				AlterarPanel.add(comboConsulta);
+				
+				
+				List<Consulta> consultas = null;
+				try {
+					consultas = ConsultaDAO.getInstance().selectAllCon();
+				} catch (SelectException e2) {
+					e2.printStackTrace();
+				} 
+				comboConsulta.setModel(new ComboBoxModelAll(consultas));
+				
+				
+				btnAlterar.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					Consulta consulta = new Consulta();
+					String aux;
+					 aux = valorField.getText();
+					 int valor = Integer.parseInt(aux);
+					 consulta.setValor(valor);
+					 aux = horarioField.getText();
+					 
+					 SimpleDateFormat dtf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+						Date dataFormatada;
+						try {
+							dataFormatada = dtf.parse(aux);
+							 consulta.getHorario().setTime(dataFormatada);
+						} catch (ParseException e1) {
+							System.out.println("erro de formatação da data");
+							return;
+						}
+						consulta.setDiagnostico(diagnosticoField.getText());
+					 
+					sistema.alterarConsulta(consulta);	
+					
+					JOptionPane.showMessageDialog(null, "Alteração efetuada com sucesso !");
+					AlterarPanel.setVisible(false);
+					}
+				});
+				btnAlterar.setBounds(686, 422, 124, 40);
+				
 			}
 		});
 		btnAlterarConsulta.setBounds(10, 398, 120, 30);
@@ -546,7 +804,50 @@ public class Interface {
 		btnExcluirMedico.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-			
+				
+				JPanel deletePanel = new JPanel();
+				deletePanel.setVisible(true);
+				
+				LayoutManager overlay = new OverlayLayout(deletePanel);
+				deletePanel.setLayout(overlay);
+				JButton btnRemover = new JButton("Remover");
+				deletePanel.add(btnRemover);
+				btnRemover.setVisible(true);
+				
+				deletePanel.setBounds(154, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(deletePanel);
+				deletePanel.setLayout(null);
+				
+				comboMedico = new JComboBox();
+				comboMedico.setBounds(10, 10, 330, 30);
+				deletePanel.add(comboMedico);
+				
+				
+				List<Medico> medicos = null;
+				try {
+					medicos = MedicoDAO.getInstance().selectAllMed();
+				} catch (SelectException e2) {
+					e2.printStackTrace();
+				} 
+				comboMedico.setModel(new ComboBoxModelAll(medicos));
+				
+				btnRemover.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					Medico medico = new Medico();
+					medico = (Medico) comboMedico.getModel().getSelectedItem();
+						
+					
+					sistema.removerMedico(medico.getId());
+					sistema.removerPessoa(medico.getIdPes());
+					
+						JOptionPane.showMessageDialog(null, "Remoção efetuada com sucesso !");
+						deletePanel.setVisible(false);
+						btnRemover.setVisible(false);
+					}
+				});
+				btnRemover.setBounds(686, 422, 124, 40);
 			}
 		});
 		btnExcluirMedico.setBounds(10, 480, 120, 30);
@@ -558,6 +859,50 @@ public class Interface {
 		btnExcluirConsulta.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				JPanel deletePanel = new JPanel();
+				deletePanel.setVisible(true);
+				
+				LayoutManager overlay = new OverlayLayout(deletePanel);
+				deletePanel.setLayout(overlay);
+				JButton btnRemover = new JButton("Remover");
+				deletePanel.add(btnRemover);
+				btnRemover.setVisible(true);
+				
+				deletePanel.setBounds(154, 70, 820, 481);
+				frmHospitalSystem.getContentPane().add(deletePanel);
+				deletePanel.setLayout(null);
+				
+				comboConsulta = new JComboBox();
+				comboConsulta.setBounds(10, 10, 800, 30);
+				deletePanel.add(comboConsulta);
+				
+				
+				List<Consulta> consulta = null;
+				try {
+					consulta = ConsultaDAO.getInstance().selectAllCon();
+				} catch (SelectException e2) {
+					e2.printStackTrace();
+				} 
+				comboConsulta.setModel(new ComboBoxModelAll(consulta));
+				
+				btnRemover.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+					Consulta consulta = new Consulta();
+					consulta = (Consulta) comboConsulta.getModel().getSelectedItem();
+						
+					
+					sistema.removerConsulta(consulta.getId());
+			
+					
+						JOptionPane.showMessageDialog(null, "Remoção efetuada com sucesso !");
+						deletePanel.setVisible(false);
+						btnRemover.setVisible(false);
+					}
+				});
+				btnRemover.setBounds(686, 422, 124, 40);
 			}
 		});
 		btnExcluirConsulta.setBounds(10, 521, 120, 30);
@@ -608,6 +953,28 @@ public class Interface {
 			}
 		}
 		
+		public void atualizaTabelaC(ArrayList<Consulta> lista) {
+			limpaTabela(tblConsultas);
+			String linha[] = new String[] {"", "", "", "",""};
+			try {
+				DefaultTableModel dadosCon = (DefaultTableModel) tblConsultas.getModel();
+				
+				int pos = -1;
+				for(Consulta consulta : lista) {
+					pos++;
+					dadosCon.addRow(linha);
+					dadosCon.setValueAt(consulta.getId(), pos, 0);
+					dadosCon.setValueAt(consulta.getValor(), pos, 1);
+					dadosCon.setValueAt(consulta.getHorario().getTime(), pos, 2);
+					dadosCon.setValueAt(consulta.getMedico().getId(), pos, 3);
+					dadosCon.setValueAt(consulta.getPaciente().getId(), pos, 4);
+
+
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Ocorreu um erros: " + e.getMessage(), "Erro:", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 
 		public void limpaTabela(JTable table) {
 			DefaultTableModel dtm = (DefaultTableModel) table.getModel();
